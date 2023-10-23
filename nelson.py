0bf49db5dc3b464081a5ab9505fba9f7d035c327
Nelsonsiburian <147901751+Nelsonsiburian@users.noreply.github.com>
2023-10-23 20:14:32 +0700

    Add files via upload

---
+++ nelson.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+hasil = a + b
+print("Hasil penjumlahan a dan b adalah:", hasil)
+x = 9
+y = 8
+hasil = x * y
+print("Hasil perkalian x dan y adalah:", hasil)
+nilai1 = 95
+nilai2 = 85
+nilai3 = 92
+rata_rata = (nilai1 + nilai2 + nilai3)
+print("rata_rata dari ketiga nilai adalah:", hasil)
