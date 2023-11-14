d5e9da1da90061b4a01b8717844ba1465cb61601
Nelsonsiburian <147901751+Nelsonsiburian@users.noreply.github.com>
2023-11-14 21:44:40 +0700

    Add files via upload

---
+++ tugas11.py
@@ -0,0 +1,14 @@
+nama_buah = ["mangga", "salak", "pisang", "mangga", "salak", "durian"]
+print(nama_buah)
+
+#list len
+nama_kota = ["jakarta", "bogor", "bandung", "medan"]
+print(len(nama_kota))
+
+#list string
+list1 = ["dandi", "nelson", "oom", "hellen"]
+list2 = [1,2,3,4,5,6,7,8,9]
+list3 = [True, False, True, False]
+print(list1)
+print(list2)
+print(list3)
