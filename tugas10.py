0447fc6402a8267128c716823228c273b578ca2c
Nelsonsiburian <147901751+Nelsonsiburian@users.noreply.github.com>
2023-11-14 21:25:09 +0700

    Add files via upload

---
+++ tugas10.py
@@ -0,0 +1,8 @@
+#list
+nama = ("dandi","nelson","wesly","rimson")
+print(nama)
+
+list = [i for i in range(0,10) if i !=5]
+print(list)
+list_kota = ["medan","jakarta","bandung","lampung"]
+print(list_kota)
